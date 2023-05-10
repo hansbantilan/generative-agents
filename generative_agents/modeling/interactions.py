275a1c0d0bbee65c9549abc2ce294f9dc52e43a0
Hans Bantilan <hansbantilan@gmail.com>
2023-05-10 17:43:11 +0100

    add GPT-Interviewer

--- generative_agents/modeling/interactions.py
+++ generative_agents/modeling/interactions.py
@@ -1,13 +1,15 @@
-from generative_agents.modeling.langchain_agent import GenerativeAgent
 from typing import List
 
+from generative_agents.modeling.langchain_agent import GenerativeAgent
 
-user_name = "superuser"
+user_name = "GPT-Interviewer"
+
 
 def interview_agent(agent: GenerativeAgent, message: str) -> str:
     """Help the user interact with the agent."""
     new_message = f"{user_name} says {message}"
     return agent.generate_dialogue_response(new_message)[1]
+
 
 def run_conversation(agents: List[GenerativeAgent], initial_observation: str) -> None:
     """Runs a conversation between agents."""
@@ -17,7 +19,9 @@ def run_conversation(agents: List[GenerativeAgent], initial_observation: str) ->
     while True:
         break_dialogue = False
         for agent in agents:
-            stay_in_dialogue, observation = agent.generate_dialogue_response(observation)
+            stay_in_dialogue, observation = agent.generate_dialogue_response(
+                observation
+            )
             print(observation)
             # observation = f"{agent.name} said {reaction}"
             if not stay_in_dialogue:
